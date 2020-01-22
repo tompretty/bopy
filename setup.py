541dd5e8da93ddf3fd84286f72c9e48aff707a3a
TomPretty <tompretty2@gmail.com>
2020-01-22 16:37:34 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="bopy",
+    author="Tom Pretty",
+    author_email="tpretty@robots.ox.ac.uk",
+    license="MIT",
+    packages=["bopy"],
+    zip_safe=False,
+)

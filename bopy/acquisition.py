3ba233f68ace764b493cbc0d9aed359939804017
TomPretty <tompretty2@gmail.com>
2020-01-27 12:57:32 +0000

    Add fit to acquisition functions

--- bopy/acquisition.py
+++ bopy/acquisition.py
@@ -1,22 +1,63 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 from scipy.stats import norm
 
+from .exceptions import NotFittedError
 from .surrogate import Surrogate
 
 
-class AcquisitionFunction:
+class AcquisitionFunction(ABC):
     """An acquisition function.
 
     Acquisition functions navigate the exploration-exploitation
     trade off during optimization.
     """
 
+    def __init__(self):
+        self.is_fitted = False
+
     def __call__(self, surrogate: Surrogate, x: np.ndarray) -> np.ndarray:
-        """Evaluate the acquisition function at `x` with `surrogate`."""
+        """Evaluate the acquisition function.
+
+        Parameters
+        ----------
+        surrogate: Surrogate
+            The surrogate model.
+        x: np.ndarray of shape (n_samples, n_dimensions)
+            The input locations at which to evaluate
+            the acquisition function.
+
+        Returns
+        -------
+        a_x: np.ndarray of shape (n_samples,)
+            The value of the acquisition function
+            at `x`.
+        """
+        if not self.is_fitted:
+            raise NotFittedError("must call fit before evaluating.")
+
         return self._f(*surrogate.predict(x))
 
+    @abstractmethod
     def _f(self, mean: np.ndarray, sigma: np.ndarray) -> np.ndarray:
         raise NotImplementedError
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
+        """Fit the acquistion function to training data.
+
+        Parameters
+        ----------
+        x: np.ndarray of shape (n_samples, n_dimensions)
+            The training input.
+        y: np.ndarray of shape (n_samples,)
+            The training output.
+        """
+        self._fit(x, y)
+        self.is_fitted = True
+
+    def _fit(self, x: np.ndarray, y: np.ndarray) -> None:
+        pass
 
 
 class UCB(AcquisitionFunction):
@@ -32,6 +73,7 @@ class UCB(AcquisitionFunction):
     """
 
     def __init__(self, kappa: float = 2.0):
+        super().__init__()
         self.kappa = kappa
 
     def _f(self, mean: np.ndarray, sigma: np.ndarray) -> np.ndarray:
@@ -46,7 +88,8 @@ class EI(AcquisitionFunction):
     """
 
     def __init__(self):
-        self._eta = 0
+        super().__init__()
+        self._eta = np.inf
 
     def _f(self, mean: np.ndarray, sigma: np.ndarray):
         var = np.diag(sigma)
@@ -55,6 +98,9 @@ class EI(AcquisitionFunction):
         return -var * norm.pdf(self._eta, loc=mean, scale=std) + (
             mean - self._eta
         ) * norm.cdf(self._eta, loc=mean, scale=std)
+
+    def _fit(self, x: np.ndarray, y: np.ndarray) -> None:
+        self._eta = np.min(y)
 
 
 class POI(AcquisitionFunction):
@@ -65,10 +111,14 @@ class POI(AcquisitionFunction):
     """
 
     def __init__(self):
-        self._eta = 0
+        super().__init__()
+        self._eta = np.inf
 
     def _f(self, mean: np.ndarray, sigma: np.ndarray):
         var = np.diag(sigma)
         std = np.sqrt(var)
 
         return -norm.cdf(self._eta, mean, std)
+
+    def _fit(self, x: np.ndarray, y: np.ndarray) -> None:
+        self._eta = np.min(y)
--- bopy/test_acquisition.py
+++ bopy/test_acquisition.py
@@ -5,20 +5,40 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import Matern
 
 from bopy.acquisition import EI, POI, UCB
+from bopy.exceptions import NotFittedError
 from bopy.surrogate import ScipyGPSurrogate
 
 
 @pytest.mark.parametrize("acquisition", [EI(), UCB(), POI()])
-def test_output_dimensions_are_correct(acquisition):
+def test_fit_must_be_called_before_evaluating(acquisition):
     # ARRANGE
     n_dimensions = 1
-    n_samples = 100
+    n_samples = 10
 
     x, y = make_regression(n_samples, n_dimensions)
 
     gp = GaussianProcessRegressor(kernel=Matern(nu=1.5))
     surrogate = ScipyGPSurrogate(gp=gp)
     surrogate.fit(x, y)
+
+    # ACT/ASSERT
+    with pytest.raises(NotFittedError, match="must call fit before evaluating."):
+        acquisition(surrogate, x)
+
+
+@pytest.mark.parametrize("acquisition", [EI(), UCB(), POI()])
+def test_output_dimensions_are_correct(acquisition):
+    # ARRANGE
+    n_dimensions = 1
+    n_samples = 10
+
+    x, y = make_regression(n_samples, n_dimensions)
+
+    gp = GaussianProcessRegressor(kernel=Matern(nu=1.5))
+    surrogate = ScipyGPSurrogate(gp=gp)
+
+    surrogate.fit(x, y)
+    acquisition.fit(x, y)
 
     # ACT
     a_x = acquisition(surrogate, x)

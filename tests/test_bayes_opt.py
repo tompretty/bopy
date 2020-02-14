890273d78dfcadb8d5460487b9f9fdd5e96316c2
TomPretty <tompretty2@gmail.com>
2020-02-14 14:27:46 +0000

    Refactor bo into fixture and swap to GPyGPSurrogate + UniformRandomInitialDesign

--- tests/test_bayes_opt.py
+++ tests/test_bayes_opt.py
@@ -1,27 +1,37 @@
+import GPy
 import numpy as np
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import Matern
+import pytest
 
 from bopy.acquisition import LCB
 from bopy.bayes_opt import BayesOpt
 from bopy.benchmark_functions import forrester
 from bopy.bounds import Bound, Bounds
-from bopy.initial_design import SobolSequenceInitialDesign
+from bopy.initial_design import UniformRandomInitialDesign
 from bopy.optimizer import DirectOptimizer
-from bopy.surrogate import ScipyGPSurrogate
+from bopy.surrogate import GPyGPSurrogate
 
 
-def test_run_initial_design_results_in_correct_shaped_dataset():
-    # ARRANGE
-    bo = BayesOpt(
+@pytest.fixture
+def bo():
+    def gp_initializer(x, y):
+        return GPy.models.GPRegression(
+            x, y, kernel=GPy.kern.RBF(input_dim=1), noise_var=1e-5, normalizer=True
+        )
+
+    surrogate = GPyGPSurrogate(gp_initializer=gp_initializer)
+
+    return BayesOpt(
         objective_function=forrester,
-        surrogate=ScipyGPSurrogate(gp=GaussianProcessRegressor(kernel=Matern(nu=1.5))),
+        surrogate=surrogate,
         acquisition_function=LCB(),
-        optimizer=DirectOptimizer(),
-        initial_design=SobolSequenceInitialDesign(),
+        optimizer=DirectOptimizer(maxf=100),
+        initial_design=UniformRandomInitialDesign(),
         bounds=Bounds(bounds=[Bound(lower=0.0, upper=1.0)]),
     )
 
+
+def test_run_initial_design_results_in_correct_shaped_dataset(bo):
+    # ARRANGE
     n_initial_design = 5
 
     # ACT
@@ -32,17 +42,8 @@ def test_run_initial_design_results_in_correct_shaped_dataset():
     assert bo.y.shape == (n_initial_design,)
 
 
-def test_run_trial_results_in_correct_shaped_dataset():
+def test_run_trial_results_in_correct_shaped_dataset(bo):
     # ARRANGE
-    bo = BayesOpt(
-        objective_function=forrester,
-        surrogate=ScipyGPSurrogate(gp=GaussianProcessRegressor(kernel=Matern(nu=1.5))),
-        acquisition_function=LCB(),
-        optimizer=DirectOptimizer(maxf=100),
-        initial_design=SobolSequenceInitialDesign(),
-        bounds=Bounds(bounds=[Bound(lower=0.0, upper=1.0)]),
-    )
-
     n_initial_design = 5
 
     # ACT
@@ -54,17 +55,22 @@ def test_run_trial_results_in_correct_shaped_dataset():
     assert bo.y.shape == (n_initial_design + 1,)
 
 
-def test_append_to_dataset():
+def test_run_trials_results_in_correct_shaped_dataset(bo):
     # ARRANGE
-    bo = BayesOpt(
-        objective_function=forrester,
-        surrogate=ScipyGPSurrogate(gp=GaussianProcessRegressor(kernel=Matern(nu=1.5))),
-        acquisition_function=LCB(),
-        optimizer=DirectOptimizer(),
-        initial_design=SobolSequenceInitialDesign(),
-        bounds=Bounds(bounds=[Bound(lower=0.0, upper=1.0)]),
-    )
+    n_initial_design = 5
+    n_trials = 2
 
+    # ACT
+    bo.run_initial_design(n_initial_design)
+    bo.run_trials(n_trials)
+
+    # ASSERT
+    assert bo.x.shape == (n_initial_design + n_trials, 1)
+    assert bo.y.shape == (n_initial_design + n_trials,)
+
+
+def test_append_to_dataset(bo):
+    # ARRANGE
     n_samples = 100
     n_dimensions = 1
 

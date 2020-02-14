29494c4ad17e3418831db48b56e6cfea537a629d
TomPretty <tompretty2@gmail.com>
2020-02-14 18:21:43 +0000

    Add docstrings.

--- bopy/callback.py
+++ bopy/callback.py
@@ -2,11 +2,45 @@ from .optimizer import OptimizationResult
 
 
 class Callback:
+    """A Callback.
+
+    Callbacks are called after certain events happen
+    during the optimization. They provide a means to 
+    execute arbitrary code (e.g. making some plots) at
+    intermediary stages in the optimization.
+
+    Custom callbacks should subclass Callback and 
+    overwrite the events they are interested in.
+    """
+
     def on_acquisition_optimized(self, bo, opt_result: OptimizationResult):
+        """Event raised after the acquisition function has been updated.
+        
+        Parameters
+        ----------
+        bo : BayesOpt
+            The BayesOpt object.
+        opt_result : OptimizationResult
+            The result from optimizing the acquisition function.
+        """
         pass
 
     def on_surrogate_updated(self, bo):
+        """Event raised after the surrogate has been updated.
+        
+        Parameters
+        ----------
+        bo : BayesOpt
+            The BayesOpt object.
+        """
         pass
 
     def on_acquisition_updated(self, bo):
+        """Event raised after the acquisition function has been updated.
+        
+        Parameters
+        ----------
+        bo : BayesOpt
+            The BayesOpt object.
+        """
         pass

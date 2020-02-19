026bffd7ddf09c58027dd0c40c9765a40c281120
TomPretty <tompretty2@gmail.com>
2020-02-19 22:13:01 +0000

    Swap to install_requires over Pipfile

--- setup.py
+++ setup.py
@@ -6,5 +6,15 @@ setup(
     author_email="tpretty@robots.ox.ac.uk",
     license="MIT",
     packages=["bopy"],
+    install_requires=[
+        "numpy",
+        "sobol-seq",
+        "pydoe",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "gpy",
+        "scipydirect",
+    ],
     zip_safe=False,
 )

ecf7e2010093ab3cf55406f6c9aa5218f8a2ec21
TomPretty <tompretty2@gmail.com>
2020-02-19 22:51:38 +0000

    Remove previous changes as they didn't work

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import Distribution, setup
 
-Distribution().fetch_build_eggs(["numpy"])
-
 setup(
     name="bopy",
     author="Tom Pretty",
     author_email="tpretty@robots.ox.ac.uk",
     license="MIT",
     packages=["bopy"],
-    setup_requires=["numpy"],
     install_requires=[
         "numpy",
         "sobol-seq",

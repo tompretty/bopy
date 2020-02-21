7aa03b3e60512940be7d11bf2c5963e63f9270ad
TomPretty <tompretty2@gmail.com>
2020-02-21 14:07:54 +0000

    Refactor test_surrogate + remove some GPy tests

--- tests/test_surrogate.py
+++ tests/test_surrogate.py
@@ -8,12 +8,17 @@ from bopy.benchmark_functions import forrester
 from bopy.exceptions import NotFittedError
 from bopy.surrogate import GPyGPSurrogate, ScipyGPSurrogate
 
+n_samples = 10
 
-def make_dataset(n_samples):
-    x = np.random.uniform(size=(n_samples, 1))
-    y = forrester(x)
 
-    return x, y
+@pytest.fixture(scope="module", autouse=True)
+def x():
+    return np.linspace(0, 1, n_samples).reshape(-1, 1)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def y(x):
+    return forrester(x)
 
 
 def scipy_gp_surrogate():
@@ -31,223 +36,92 @@ def gpy_gp_surrogate():
     return GPyGPSurrogate(gp_initializer=gp_initializer)
 
 
-def noise_free_gpy_gp_surrogate():
-    def gp_initializer(x, y):
-        gp = GPy.models.GPRegression(
-            x, y, kernel=GPy.kern.RBF(input_dim=1), noise_var=1e-10, normalizer=True
-        )
-        gp.Gaussian_noise.fix()
-
-        return gp
-
-    return GPyGPSurrogate(gp_initializer=gp_initializer)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_training_input_must_contain_at_least_one_sample(surrogate):
-    # ARRANGE
-    x, y = np.array([]), np.array([])
-
-    # ACT/ASSERT
-    with pytest.raises(ValueError, match="`x` must contain at least one sample"):
-        surrogate.fit(x, y)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_training_target_must_contain_at_least_one_sample(surrogate):
-    # ARRANGE
-    x, y = np.array([1.0]), np.array([])
-
-    # ACT/ASSERT
-    with pytest.raises(ValueError, match="`y` must contain at least one sample"):
-        surrogate.fit(x, y)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_training_input_and_target_must_be_the_same_size(surrogate):
-    # ARRANGE
-    n_samples_x = 15
-    n_samples_y = 5
-
-    x, y = make_dataset(n_samples=n_samples_x)
-    y = y[:n_samples_y]
-
-    # ACT/ASSERT
-    with pytest.raises(
-        ValueError, match="`x` and `y` must have the same number of samples"
-    ):
-        surrogate.fit(x, y)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_training_input_must_be_2d(surrogate):
-    # ARRANGE
-    x, y = np.array([[[1]]]), np.array([1])
-
-    # ACT/ASSERT
-    with pytest.raises(ValueError, match="`x` must be 2D"):
-        surrogate.fit(x, y)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_training_target_must_be_1d(surrogate):
-    # ARRANGE
-    x, y = np.array([[1]]), np.array([[1]])
-
-    # ACT/ASSERT
-    with pytest.raises(ValueError, match="`y` must be 1D"):
-        surrogate.fit(x, y)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_fit_must_be_called_before_predict(surrogate):
-    # ARRANGE
-    n_samples = 100
-
-    x, y = make_dataset(n_samples=n_samples)
-
-    # ACT/ASSERT
-    with pytest.raises(NotFittedError, match="must be fitted first"):
-        surrogate.predict(x)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_predict_returns_correct_dimensions(surrogate):
-    # ARRANGE
-    n_samples = 100
-
-    x, y = make_dataset(n_samples=n_samples)
-
-    surrogate.fit(x, y)
-
-    # ACT
-    y_pred, sigma = surrogate.predict(x)
-
-    # ASSERT
-    assert y_pred.shape == (n_samples,)
-    assert sigma.shape == (n_samples, n_samples)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_test_input_contains_at_least_one_sample(surrogate):
-    # ARRANGE
-    n_samples = 100
-
-    x_train, y_train = make_dataset(n_samples=n_samples)
-    x_test = np.array([])
-
-    surrogate.fit(x_train, y_train)
-
-    # ACT/ASSERT
-    with pytest.raises(ValueError, match="`x` must contain at least one sample"):
-        surrogate.predict(x_test)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_test_input_must_be_2d(surrogate):
-    # ARRANGE
-    n_samples = 10
-
-    x_train, y_train = make_dataset(n_samples=n_samples)
-    x_test = np.zeros((n_samples, 1, 1))
-
-    surrogate.fit(x_train, y_train)
-
-    # ACT/ASSERT
-    with pytest.raises(ValueError, match="`x` must be 2D"):
-        surrogate.predict(x_test)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_surrogate_updates_data_references(surrogate):
-    # ARRANGE
-    n_samples = 10
-
-    x_train, y_train = make_dataset(n_samples=n_samples)
-
-    # ACT
-    surrogate.fit(x_train, y_train)
-
-    # ASSERT
-    assert np.array_equal(x_train, surrogate.x)
-    assert np.array_equal(y_train, surrogate.y)
-
-
-@pytest.mark.parametrize("surrogate", [scipy_gp_surrogate(), gpy_gp_surrogate()])
-def test_test_input_must_have_same_number_of_dimensions_as_training_input(surrogate):
-    # ARRANGE
-    n_dimensions_train = 1
-    n_samples_train = 10
-
-    n_dimensions_test = 2
-    n_samples_test = 10
-
-    x_train, y_train = make_dataset(n_samples_train)
-    x_test = np.random.uniform(size=(n_samples_test, n_dimensions_test))
-
-    surrogate.fit(x_train, y_train)
-
-    # ACT/ASSERT
-    with pytest.raises(
-        ValueError,
-        match="`x` must have the same number of dimensions as the training data",
-    ):
-        surrogate.predict(x_test)
-
-
-@pytest.mark.parametrize(
-    "surrogate", [scipy_gp_surrogate(), noise_free_gpy_gp_surrogate()]
+@pytest.fixture(
+    scope="module",
+    autouse=True,
+    params=[scipy_gp_surrogate(), gpy_gp_surrogate()],
+    ids=["scipy_gp", "gpy_gp"],
 )
-def test_noise_free_gps_interpolate_the_training_data(surrogate):
-    # ARRANGE
-    n_samples = 5
+def surrogate(request):
+    return request.param
 
-    x, y = make_dataset(n_samples)
+
+@pytest.fixture(scope="class")
+def trained_surrogate(surrogate, x, y):
     surrogate.fit(x, y)
-
-    # ACT
-    y_pred, sigma = surrogate.predict(x)
-    var = np.diag(sigma)
-
-    # ASSERT
-    assert np.allclose(y, y_pred, atol=1e-3)
-    assert np.allclose(0, var, atol=1e-3)
+    return surrogate
 
 
-def test_GPyGPSurrogate_instantiates_a_gp_after_calling_fit():
-    # ARRANGE
-    n_dimensions = 1
-    n_samples = 10
+class TestArgumentsToFit:
+    def test_x_must_contain_at_least_one_sample(self, surrogate):
+        with pytest.raises(ValueError, match="`x` must contain at least one sample"):
+            surrogate.fit(x=np.array([]), y=np.array([1.0]))
 
-    x = np.linspace(0.0, 1.0, n_samples).reshape(-1, 1)
-    y = np.sin(x.flatten())
+    def test_y_must_contain_at_least_one_sample(self, surrogate):
+        with pytest.raises(ValueError, match="`y` must contain at least one sample"):
+            surrogate.fit(x=np.array([[1.0]]), y=np.array([]))
 
-    surrogate = gpy_gp_surrogate()
+    def test_x_and_y_must_contain_the_same_number_of_samples(self, surrogate):
+        with pytest.raises(
+            ValueError, match="`x` and `y` must have the same number of samples"
+        ):
+            surrogate.fit(x=np.array([[1.0]]), y=np.array([1.0, 1.0]))
 
-    # ACT
-    surrogate.fit(x, y)
+    def test_x_must_be_2d(self, surrogate):
+        with pytest.raises(ValueError, match="`x` must be 2D"):
+            surrogate.fit(x=np.array([[[1.0]]]), y=np.array([1.0]))
 
-    # ASSERT
-    assert surrogate.gp is not None
+    def test_y_must_be_1d(self, surrogate):
+        with pytest.raises(ValueError, match="`y` must be 1D"):
+            surrogate.fit(x=np.array([[1.0]]), y=np.array([[1.0]]))
 
 
-def test_GPyGPSurrogate_updates_data_on_second_call_to_fit():
-    # ARRANGE
-    n_dimensions = 1
-    n_samples = 10
+class TestBeforeFitting:
+    def test_calling_predict_raises_not_fitted_error(self, surrogate, x):
+        with pytest.raises(NotFittedError, match="must be fitted first"):
+            surrogate.predict(x)
 
-    x1 = np.linspace(0.0, 1.0, n_samples).reshape(-1, 1)
-    y1 = np.sin(x1.flatten())
 
-    x2 = np.linspace(1.0, 2.0, n_samples).reshape(-1, 1)
-    y2 = np.sin(x2.flatten())
+class TestArgumentsToPredictAfterFitting:
+    def test_x_must_contain_at_least_one_sample(self, trained_surrogate):
+        with pytest.raises(ValueError, match="`x` must contain at least one sample"):
+            trained_surrogate.predict(x=np.array([]))
 
-    surrogate = gpy_gp_surrogate()
-    surrogate.fit(x1, y1)
+    def test_x_must_be_2d(self, trained_surrogate):
+        with pytest.raises(ValueError, match="`x` must be 2D"):
+            trained_surrogate.predict(x=np.array([1.0]))
 
-    # ACT
-    surrogate.fit(x2, y2)
+    def test_x_must_have_the_same_number_of_dimensions_as_the_training_data(
+        self, trained_surrogate
+    ):
+        with pytest.raises(
+            ValueError,
+            match="`x` must have the same number of dimensions as the training data",
+        ):
+            trained_surrogate.predict(x=np.array([[1.0, 1.0]]))
 
-    # ASSERT
-    assert np.array_equal(surrogate.gp.X, x2)
-    assert np.array_equal(surrogate.gp.Y, y2.reshape(-1, 1))
+
+class TestAfterPredicting:
+    @pytest.fixture(scope="class", autouse=True)
+    def predictions(self, trained_surrogate, x):
+        return trained_surrogate.predict(x)
+
+    @pytest.fixture(scope="class", autouse=True)
+    def predicted_mean(self, predictions):
+        return predictions[0]
+
+    @pytest.fixture(scope="class", autouse=True)
+    def predicted_var(self, predictions):
+        return predictions[1]
+
+    def test_predicted_mean_is_the_correct_shape(self, predicted_mean):
+        assert predicted_mean.shape == (n_samples,)
+
+    def test_predicted_var_is_the_correct_shape(self, predicted_var):
+        assert predicted_var.shape == (n_samples, n_samples)
+
+    def test_reference_to_x_is_stored(self, trained_surrogate, x):
+        assert np.array_equal(trained_surrogate.x, x)
+
+    def test_reference_to_y_is_stored(self, trained_surrogate, y):
+        assert np.array_equal(trained_surrogate.y, y)
